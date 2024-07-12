8c02542b7d346f4e9e2842cc227bba97d859d306
Kenneth <kennethmarcano96@outlook.com>
2024-07-12 14:25:24 -0300

    'whitelistCors-update'

--- api_rest_users_alunos/src/app.js
+++ api_rest_users_alunos/src/app.js
@@ -17,7 +17,7 @@ import alunoRoutes from './routes/alunoRoutes'
 import fotoRoutes from './routes/fotoRoutes'
 
 const whiteList = [
-  'https://student-control.vercel.app',
+  'https://student-control.vercel.app/',
   'https://student-control.vercel.app/:1',
   'http://35.199.67.52:81',
   'http://35.199.67.52:3003',
