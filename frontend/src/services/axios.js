4e1413041eee9be04685050a08b0c535ae50a0e4
Kenneth <kennethmarcano96@outlook.com>
2024-07-12 14:15:59 -0300

    'update-axiosCreate-frontend'

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: 'http://35.199.67.52:81',
+    headers: {
+        'Origin': 'https://student-control.vercel.app'
+      }
 })
